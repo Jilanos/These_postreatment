44dc43f792d7a3ed21a21aaf9b6890280a6cadb5
Jilanos <paul.mondou@live.fr>
2023-06-06 11:17:33 +0200

    premiere version operationnelle de l ia de prediction de concentration

--- bulles_concentration.py
+++ bulles_concentration.py
@@ -8,6 +8,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 import sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from sklearn.linear_model import LinearRegression
+from sklearn.neighbors import NearestNeighbors, KNeighborsRegressor
+from sklearn.metrics import r2_score
 from classes import *
 from scipy import signal
 import sys 
@@ -18,7 +23,7 @@ gc.collect(generation=2)
 pc_fixe = False
 if pc_fixe:
     tra = 'D:\\code_UH_long\\GENE_MOD\\iter_13\\'
-    traj='C:\\Users\\MIDAS\\Desktop\\resultats_labelling\\iter_13\\'
+    traj='C:\\Users\\MIDAS\\Desktop\\resultats_concentration\\iter_13\\'
     doss=["PULSE_0_75","PULSE_240_75","PULSE_80_75","PULSE_27_75"]
     doss=["bubbles_0_75","bubbles_240_75","bubbles_80_75","bubbles_27_75"] 
     legend=["Eau","Dilution 1:240","Dilution 1:80","Dilution 1:27"]
@@ -26,7 +31,7 @@ if pc_fixe:
     rep = 20
 else:
     tra = 'C:\\Users\\PM263553\\Desktop\\These\\big_projects\\in_vitro\\iter_19\\'
-    traj = 'C:\\Users\\PM263553\\Desktop\\These\\big_projects\\in_vitro\\iter_19\\map_sklearn\\'
+    traj = 'C:\\Users\\PM263553\\Desktop\\These\\big_projects\\in_vitro\\iter_19\\resultats_concentration\\'
     doss=["PULSE_0_40","PULSE_666_40"]
     legend=["Eau","Dilution 1:666"]
     nexp=2
@@ -66,9 +71,6 @@ for i in range(0,nexp):
     gc.collect(generation=2)
 
 #%%
-from sklearn.cluster import KMeans
-from sklearn.cluster import DBSCAN
-from sklearn.mixture import GaussianMixture
 k=5
 # on cree la carte de cavitation
 # les dimensions de la carte sont : nbr d'indice, nbr d'exp, nbr de pulses, nbr de fenetres, nbr de composantes
@@ -89,12 +91,91 @@ n_exp = shape[0]
 n_pulse = shape[1]
 n_fen = shape[2]
 last_dim = [shape[2], shape[3],shape[4],shape[5]]
-X_inter = np.reshape(carte_reshape,(shape[0]*shape[1]*shape[2],shape[3],shape[4],shape[5]))
-# il faut donc réaplatire les dimensions 1,2,3 pour pouvoir appliquer l'algo de labelisation
-shape = np.shape(X_inter)
+print("shape de carte_reshape : ",shape)
+# il faut donc réaplatire les dimensions 3,4,5
+X_plat = np.reshape(carte_reshape,(shape[0],shape[1],shape[2],shape[3]*shape[4]*shape[5]))
+# (exp, pulses, fenetres//k, k*indice*composantes)
+# on veut maintenant rajouter la pression dans la dernière dimension
+x_pre = np.zeros((n_exp,n_pulse,n_fen,k*shape[4]*shape[5]+1))
+x_pre[:,:,:,:-1] = X_plat
+# on va maintenant rajouter la pression dans la dernière dimension
+for i_exp in range(n_exp):
+    for i_pulse in range(n_pulse):
+        for i_fen in range(n_fen):
+            x_pre[i_exp,i_pulse,i_fen,-1] = i_pulse//rep *fit[0] + fit[1]
+# on veut écrire une fct qui va nous donner les valeure suivantes : i = 0 : 0; i = 1 : 1, i = 2 : 3, i = 3 : 9, i = 4 : 27, ...
+def puissance(i):
+    if i==0:
+        return 0
+    else:
+        return 3**(i-1) 
 
-X = np.reshape(X_inter,(shape[0],shape[1]*shape[2]*shape[3]))
-shape = np.shape(X)
+#on va créer l'array de sortie y :
+y_pre = np.zeros((n_exp,n_pulse,n_fen))
+for i_exp in range(n_exp):
+    print("exp : ",i_exp," concentration : ",puissance(i_exp))
+    for i_pulse in range(n_pulse):
+        for i_fen in range(n_fen):
+            y_pre[i_exp,i_pulse,i_fen] = puissance(i_exp)
+
+# on va fusionner les 3 première dimensions
+X_final = np.reshape(x_pre,(n_exp*n_pulse*n_fen,k*shape[4]*shape[5]+1))
+Y_final = np.reshape(y_pre,(n_exp*n_pulse*n_fen))
+shape_X = np.shape(X_final)
+shape_Y = np.shape(Y_final)
+
+
+# on va maintenant regarder la carte de concentration créé par la régression linéaire
+def carte_concentration(data,chemin, titre,legend):
+    fig=plt.figure(figsize=(20,11))
+    fig, axes = plt.subplots(nrows=1, ncols=n_exp, figsize=(20,11))
+    fig.suptitle(titre,fontsize=18, fontweight = 'bold',y=0.94)
+    for i,ax in enumerate(axes.flat):
+        im = ax.imshow(data[i,:,:],aspect='auto',interpolation='none',cmap='turbo',extent=[0,11.,((nbit[1])*fit[0]+fit[1]),((nbit[0])*fit[0]+fit[1])])  #,extent=[0,pulse_time_ms,seq_time_s,0]   ,mapval  , vmin=mapval[i*2],vmax=mapval[i*2+1]
+        ax.title.set_text(legend[i].upper())
+        ax.title.set_fontweight('bold')
+        ax.set_xlabel('Intra-Pulse Time'.upper()+' (ms)')
+        if i==0:
+            ax.set_ylabel("Pression (kPa)")
+            fig.colorbar(im,ax=axes.ravel().tolist())
+            #plt.savefig(chemin+"\\bulles_H{}_fonda.png".format(k),bbox_inches='tight')
+    plt.savefig(chemin+titre+".png",bbox_inches='tight')
+    plt.close('all')
+
+
+X_train, X_test, y_train, y_test = train_test_split(X_final, Y_final, train_size=0.8, test_size=0.2, random_state=42)
+print("\nModel : LinearRegression")
+lr = LinearRegression()
+lr.fit(X_train, y_train)
+y_pred = lr.predict(X_test)
+mse = mean_squared_error(y_test, y_pred)
+r2s = r2_score(y_test, y_pred)
+lrs = lr.score(X_test, y_test)
+print("MSE : ",mse)
+print("R2 : ",r2s)
+print("LR score : ",lrs)
+c_pred = lr.predict(X_final)
+carte_concentration(np.reshape(c_pred,(n_exp,n_pulse,n_fen)),traj,"LinearRegression",legend)
+
+for k in [3,6,11,20]:
+    print("\nModel : KNeighborsRegressor with k = ",k," neighbors")
+    knn = KNeighborsRegressor(n_neighbors=k)
+    knn.fit(X_train, y_train)
+    y_pred = knn.predict(X_test)
+    mse = mean_squared_error(y_test, y_pred)
+    r2s = r2_score(y_test, y_pred)
+    knns = knn.score(X_test, y_test)
+    print("MSE : ",mse)
+    print("R2 : ",r2s)
+    print("KNN score : ",knns)
+    c_pred = knn.predict(X_final)
+    carte_concentration(np.reshape(c_pred,(n_exp,n_pulse,n_fen)),traj,"KNeighborsRegressor_{}_neighbors".format(k),legend)  
+
+
+
+sys.exit()
+
+
 
 
 def carte_cluster(data,chemin, titre,legend):
