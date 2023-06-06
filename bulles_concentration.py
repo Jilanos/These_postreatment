e51ea3d1fb64616d8cd823818cb74288a423917f
Jilanos <paul.mondou@live.fr>
2023-06-06 23:42:22 +0200

    versioning with different algo ML

--- bulles_concentration.py
+++ bulles_concentration.py
@@ -7,12 +7,12 @@ Created on Wed Apr 13 17:01:38 2022
 import numpy as np
 import matplotlib.pyplot as plt
 import os
-import sklearn
+import sklearn as skl
+from sklearn import linear_model
+from sklearn import ensemble
+from sklearn import svm
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
-from sklearn.linear_model import LinearRegression
-from sklearn.neighbors import NearestNeighbors, KNeighborsRegressor
-from sklearn.metrics import r2_score
+from sklearn.metrics import r2_score, mean_squared_error
 from classes import *
 from scipy import signal
 import sys 
@@ -20,12 +20,13 @@ import gc
 
 gc.collect(generation=2)
 
+
 pc_fixe = False
 if pc_fixe:
-    tra = 'D:\\code_UH_long\\GENE_MOD\\iter_13\\'
-    traj='C:\\Users\\MIDAS\\Desktop\\resultats_concentration\\iter_13\\'
-    doss=["PULSE_0_75","PULSE_240_75","PULSE_80_75","PULSE_27_75"]
+    tra = 'D:\\code_UH_long\\GENE_MOD\\iter_21\\'
+    traj='C:\\Users\\MIDAS\\Desktop\\resultats_concentration\\iter_21\\'
     doss=["bubbles_0_75","bubbles_240_75","bubbles_80_75","bubbles_27_75"] 
+    doss=["PULSE_0_75","PULSE_240_75","PULSE_80_75","PULSE_27_75"]
     legend=["Eau","Dilution 1:240","Dilution 1:80","Dilution 1:27"]
     nexp=4
     rep = 20
@@ -39,6 +40,40 @@ else:
 
 #VITRO
 path(traj)
+
+
+def choix_model(mod):
+    if mod =="LinearSVR" :
+        model = skl.svm.LinearSVR(max_iter=50000)
+        espacer=""
+    elif mod =="Lasso"  :
+        model = skl.linear_model.Lasso(max_iter=50000)
+        espacer="\t"
+    elif mod =="ElasticNet"  :
+        model = skl.linear_model.ElasticNet(max_iter=50000)
+        espacer=""
+    elif mod =="ElasticNetCV"  :
+        model = skl.linear_model.ElasticNetCV(max_iter=50000)
+        espacer=""
+    elif mod =="Ridge"  :
+        model = skl.linear_model.Ridge()
+        espacer="\t"
+    elif mod =="SGDRegressor"  :
+        model = skl.linear_model.SGDRegressor(max_iter=50000)
+        espacer=""
+    elif mod =="SVR"  :
+        model = skl.svm.SVR(cache_size=2000)
+        espacer="\t"
+    elif mod =="RandomForestRegressor"  :
+        model = skl.ensemble.RandomForestRegressor()
+        espacer=""
+    elif mod =="KNeighborsRegressor"  :
+        model = skl.neighbors.KNeighborsRegressor(n_neighbors = 8)
+        espacer=""
+    elif mod =="LinearRegression"  :
+        model = skl.linear_model.LinearRegression()
+        espacer=""
+    return model, espacer
 
 
 start,end = 0,-1
@@ -71,56 +106,48 @@ for i in range(0,nexp):
     gc.collect(generation=2)
 
 #%%
-k=5
+
 # on cree la carte de cavitation
 # les dimensions de la carte sont : nbr d'indice, nbr d'exp, nbr de pulses, nbr de fenetres, nbr de composantes
 # on supprime les n_supr dernieres fenetres du pulse car moins interessantes
 # cela permet de les regrouper par k
 carte = test_m_exp.extract_composante(nbit,fit,legend = legend)
 shape = np.shape(carte)
-n_supr = shape[2]%k
-carte_rognee = carte[:,:,:-n_supr,:,:]  #on supprime les fenetres en trop
-shape = np.shape(carte_rognee)
 #on regroupe les fenetres par k
 #(exp, pulses, fenetres, indice, composantes)
-carte_reshape = np.reshape(carte_rognee,(shape[0],shape[1],shape[2]//k,k,shape[3],shape[4]))
+#carte_reshape = np.reshape(carte_rognee,(shape[0],shape[1],shape[2]//k,k,shape[3],shape[4]))
 #(exp, pulses, fenetres//k, k, indice, composantes)
 #on donnera à l'algo de labelisation toutes les fenêtres, chaque point sera composé d'un lot de k fenetres et de toutes les composantes 
-shape = np.shape(carte_reshape)
 n_exp = shape[0]
 n_pulse = shape[1]
 n_fen = shape[2]
-last_dim = [shape[2], shape[3],shape[4],shape[5]]
-print("shape de carte_reshape : ",shape)
 # il faut donc réaplatire les dimensions 3,4,5
-X_plat = np.reshape(carte_reshape,(shape[0],shape[1],shape[2],shape[3]*shape[4]*shape[5]))
+X_plat = np.reshape(carte,(shape[0],shape[1],shape[2]*shape[3]*shape[4]))
 # (exp, pulses, fenetres//k, k*indice*composantes)
 # on veut maintenant rajouter la pression dans la dernière dimension
-x_pre = np.zeros((n_exp,n_pulse,n_fen,k*shape[4]*shape[5]+1))
-x_pre[:,:,:,:-1] = X_plat
+x_pre = np.zeros((n_exp,n_pulse,n_fen*shape[3]*shape[4]+1))
+x_pre[:,:,:-1] = X_plat
 # on va maintenant rajouter la pression dans la dernière dimension
 for i_exp in range(n_exp):
     for i_pulse in range(n_pulse):
-        for i_fen in range(n_fen):
-            x_pre[i_exp,i_pulse,i_fen,-1] = i_pulse//rep *fit[0] + fit[1]
+        x_pre[i_exp,i_pulse,-1] = i_pulse//rep *fit[0] + fit[1]
 # on veut écrire une fct qui va nous donner les valeure suivantes : i = 0 : 0; i = 1 : 1, i = 2 : 3, i = 3 : 9, i = 4 : 27, ...
-def puissance(i):
+def concent(i):
     if i==0:
         return 0
     else:
         return 3**(i-1) 
 
 #on va créer l'array de sortie y :
-y_pre = np.zeros((n_exp,n_pulse,n_fen))
+y_pre = np.zeros((n_exp,n_pulse))
 for i_exp in range(n_exp):
-    print("exp : ",i_exp," concentration : ",puissance(i_exp))
+    print("exp : ",i_exp," concentration : ",concent(i_exp))
     for i_pulse in range(n_pulse):
-        for i_fen in range(n_fen):
-            y_pre[i_exp,i_pulse,i_fen] = puissance(i_exp)
-
+        y_pre[i_exp,i_pulse] = concent(i_exp)
+#%%
 # on va fusionner les 3 première dimensions
-X_final = np.reshape(x_pre,(n_exp*n_pulse*n_fen,k*shape[4]*shape[5]+1))
-Y_final = np.reshape(y_pre,(n_exp*n_pulse*n_fen))
+X_final = np.reshape(x_pre,(n_exp*n_pulse,n_fen*shape[3]*shape[4]+1))
+Y_final = np.reshape(y_pre,(n_exp*n_pulse))
 shape_X = np.shape(X_final)
 shape_Y = np.shape(Y_final)
 
@@ -128,50 +155,33 @@ shape_Y = np.shape(Y_final)
 # on va maintenant regarder la carte de concentration créé par la régression linéaire
 def carte_concentration(data,chemin, titre,legend):
     fig=plt.figure(figsize=(20,11))
-    fig, axes = plt.subplots(nrows=1, ncols=n_exp, figsize=(20,11))
-    fig.suptitle(titre,fontsize=18, fontweight = 'bold',y=0.94)
-    for i,ax in enumerate(axes.flat):
-        im = ax.imshow(data[i,:,:],aspect='auto',interpolation='none',cmap='turbo',extent=[0,11.,((nbit[1])*fit[0]+fit[1]),((nbit[0])*fit[0]+fit[1])])  #,extent=[0,pulse_time_ms,seq_time_s,0]   ,mapval  , vmin=mapval[i*2],vmax=mapval[i*2+1]
-        ax.title.set_text(legend[i].upper())
-        ax.title.set_fontweight('bold')
-        ax.set_xlabel('Intra-Pulse Time'.upper()+' (ms)')
-        if i==0:
-            ax.set_ylabel("Pression (kPa)")
-            fig.colorbar(im,ax=axes.ravel().tolist())
-            #plt.savefig(chemin+"\\bulles_H{}_fonda.png".format(k),bbox_inches='tight')
+    color = ['black', 'lightcoral', 'red','darkred']
+    for i in range(np.shape(data)[0]):
+        plt.plot(data[i,:], label = legend[i], color = color[i])
+    plt.legend(fontsize = 15)
+    plt.ylabel("Concentration prédite",fontsize = 15)
+    plt.xlabel("Pulses",fontsize = 15)
+    plt.title("Variation de la concentration pour chaque concentration selon la pression", fontsize = 17)
     plt.savefig(chemin+titre+".png",bbox_inches='tight')
     plt.close('all')
 
-
-X_train, X_test, y_train, y_test = train_test_split(X_final, Y_final, train_size=0.8, test_size=0.2, random_state=42)
-print("\nModel : LinearRegression")
-lr = LinearRegression()
-lr.fit(X_train, y_train)
-y_pred = lr.predict(X_test)
-mse = mean_squared_error(y_test, y_pred)
-r2s = r2_score(y_test, y_pred)
-lrs = lr.score(X_test, y_test)
-print("MSE : ",mse)
-print("R2 : ",r2s)
-print("LR score : ",lrs)
-c_pred = lr.predict(X_final)
-carte_concentration(np.reshape(c_pred,(n_exp,n_pulse,n_fen)),traj,"LinearRegression",legend)
-
-for k in [3,6,11,20]:
-    print("\nModel : KNeighborsRegressor with k = ",k," neighbors")
-    knn = KNeighborsRegressor(n_neighbors=k)
-    knn.fit(X_train, y_train)
-    y_pred = knn.predict(X_test)
+prop_train = 0.05
+X_train, X_test, y_train, y_test = train_test_split(X_final, Y_final, train_size=prop_train, test_size=1-prop_train, random_state=42)
+models = ["Lasso","ElasticNet","ElasticNetCV","Ridge","SVR","LinearSVR","RandomForestRegressor","KNeighborsRegressor","LinearRegression"]
+for mod in models:
+    model, espacer = choix_model(mod)
+    print(mod)
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
     mse = mean_squared_error(y_test, y_pred)
     r2s = r2_score(y_test, y_pred)
-    knns = knn.score(X_test, y_test)
+    lr = model.score(X_test, y_test)
     print("MSE : ",mse)
     print("R2 : ",r2s)
-    print("KNN score : ",knns)
-    c_pred = knn.predict(X_final)
-    carte_concentration(np.reshape(c_pred,(n_exp,n_pulse,n_fen)),traj,"KNeighborsRegressor_{}_neighbors".format(k),legend)  
-
-
+    print(mod+" score : ",lr)
+    c_pred = model.predict(X_final)
+    carte_concentration(np.reshape(c_pred,(n_exp,n_pulse)),traj,mod,legend)
+    
 
 sys.exit()
 
